1014d7e0c7d0253c0dc23428d1d485af44e9f6c3
Anish George <anish.george@fabricgroup.com.au>
2024-08-19 22:34:45 +1000

    Rename of sandbox

--- apps/poral-sandbox/src/pages/_app.tsx
+++ apps/poral-sandbox/src/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to sandbox!</title>
+        <title>Welcome to portal-sandbox!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
