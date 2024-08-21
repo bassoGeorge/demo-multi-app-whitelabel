6f2e767babe0a805b81bcdb70cb1e0e3601ecb2d
Anish George <anish.george@fabricgroup.com.au>
2024-08-21 17:24:20 +1000

    Connected things through design-system and shared librar

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,2 +1,1 @@
-export * from './lib/api/useAPI';
 export * from './lib/star-services/StarServicesInfo';
--- libs/shared/src/lib/star-services/StarServicesInfo.tsx
+++ libs/shared/src/lib/star-services/StarServicesInfo.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@multi-app-whitelabel/design-system'
+
 export function StarServicesInfo() {
   return (
     <div>
       <h1>Welcome to StarServicesInfo!</h1>
+      <Button>Hey there</Button>
     </div>
   );
 }
