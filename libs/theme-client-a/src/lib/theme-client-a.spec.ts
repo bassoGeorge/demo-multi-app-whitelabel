aa355c453912e7d0ffd6b9a375e64852d6cb327d
Anish George <anish.george@fabricgroup.com.au>
2024-08-25 14:25:23 +1000

    Start setting up client A

---
+++ libs/theme-client-a/src/lib/theme-client-a.spec.ts
@@ -0,0 +1,7 @@
+import { themeClientA } from './theme-client-a';
+
+describe('themeClientA', () => {
+  it('should work', () => {
+    expect(themeClientA()).toEqual('theme-client-a');
+  });
+});
---
+++ libs/theme-client-a/src/lib/theme-client-a.ts
@@ -0,0 +1,3 @@
+export function themeClientA(): string {
+  return 'theme-client-a';
+}
