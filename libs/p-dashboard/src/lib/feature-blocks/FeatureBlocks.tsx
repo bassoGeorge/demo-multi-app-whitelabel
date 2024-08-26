d4bc9070fbdadf83c76a719ce5ab8ed1a961ec2a
Anish George <anish.george@fabricgroup.com.au>
2024-08-26 22:05:48 +1000

    Demo the assets

--- libs/p-dashboard/src/lib/feature-blocks/FeatureBlocks.tsx
+++ libs/p-dashboard/src/lib/feature-blocks/FeatureBlocks.tsx
@@ -19,7 +19,17 @@ export function FeatureBlocks() {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        Item One
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3 }}>
+          <Box>
+            <img src="/illustrations/bookshelf.svg" width={200} alt="Bookshelf" />
+          </Box>
+          <Box>
+            <img src="/illustrations/calculator.svg" width={200} alt="Calculator" />
+          </Box>
+          <Box>
+            <img src="/illustrations/pie-chart.svg" width={200} alt="Pie Chart" />
+          </Box>
+        </Box>
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
