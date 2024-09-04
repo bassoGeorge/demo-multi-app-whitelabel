e4840c3154a3f0ac53f8f702cc63c793744e8e50
Anish George <anish.george@fabricgroup.com.au>
2024-09-04 22:13:18 +1000

    Add the basic content provider

--- libs/p-dashboard/src/lib/home/Home.tsx
+++ libs/p-dashboard/src/lib/home/Home.tsx
@@ -1,9 +1,19 @@
-import { StarServicesInfo } from '@multi-app-whitelabel/shared';
+import { StarServicesInfo, useContent } from '@multi-app-whitelabel/shared';
 import { toDollarsAndCents } from '@multi-app-whitelabel/utils';
 import { Card, Typography } from '@mui/material';
 import { FeatureBlocks } from '../feature-blocks/FeatureBlocks';
+import { useEffect } from 'react';
 
 export function Home() {
+  const { common, page } = useContent();
+
+  useEffect(() => {
+    if (common && page) {
+      console.log("We have common content: ", common);
+      console.log("And page content: ", page);
+    }
+  }, [common, page])
+
   return (
     <div>
       <h1>Welcome to Home!</h1>
---
+++ libs/shared/src/lib/content/content-queris.ts
@@ -0,0 +1,40 @@
+import { QueryFunctionContext } from '@tanstack/react-query';
+import { AxiosError, AxiosInstance } from 'axios';
+
+const CONTENT_RESOURCE = '/content';
+
+export function commonContentQueryFn(api: AxiosInstance) {
+  return async function ({
+    queryKey,
+  }: QueryFunctionContext<[string, { clientId: string }]>) {
+    const [, { clientId }] = queryKey;
+
+    const { data } = await api.get(
+      `${CONTENT_RESOURCE}/${clientId}/common.json`
+    );
+
+    return data.components;
+  };
+}
+
+export function pageContentQueryFn(api: AxiosInstance) {
+  return async function ({
+    queryKey,
+  }: QueryFunctionContext<[string, { clientId: string; pageKey: string }]>) {
+    const [, { clientId, pageKey }] = queryKey;
+
+    try {
+      const { data } = await api.get(
+        `${CONTENT_RESOURCE}/${clientId}/pages/${pageKey}.json`
+      );
+
+      return data.components ?? {};
+    } catch (e: unknown) {
+      if ((e as AxiosError)?.response?.status === 404) {
+        return {}; // Some pages *might* not have content
+      } else {
+        throw e;
+      }
+    }
+  };
+}
